367ca55c1d4b0419a646c794f429df4daae4716a
suyash-mankar <suyashmankar@gmail.com>
2022-10-17 21:20:44 +0530

    setup custom fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import { getPosts } from '../api';
+
 
 function App() {
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+
+  }, []);
+
+
   return (
     <div className="App">
       <h1> working </h1>
