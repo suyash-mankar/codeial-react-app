c3e9770f6599fb458ee4262c2dfc640cc99e5954
suyash-mankar <suyashmankar@gmail.com>
2022-10-20 16:18:18 +0530

    setup login form hooks and hot-toast notifications

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
+import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <App />
+
+        <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            toastOptions={{
+                duration: 5000,
+                style: {
+                    background: '#363636',
+                    color: '#fff',
+                },
+            }}
+        />
+    </>
+
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,49 @@
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 import styles from '../styles/login.module.css';
 
-
 const Login = () => {
-    return <form className={styles.loginForm}>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loggingIn, setLoggingIn] = useState(false);
 
-        <span className={styles.loginSignupHeader}> Log In </span>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        <div className={styles.field}>
-            <input type='email' placeholder='Email' required />
-        </div>
-        <div className={styles.field}>
-            <input type='password' placeholder='Password' required />
-        </div>
+        setLoggingIn(true);
 
-        <div className={styles.field}>
-            <button> Log In </button>
-        </div>
+        if (!email || !password) {
+            toast.error('please enter both email and password');
+        }
+    }
 
-    </form>
-}
+
+    return (
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
+            <span className={styles.loginSignupHeader}> Log In </span>
+
+            <div className={styles.field}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className={styles.field}>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+
+            <div className={styles.field} disabled={loggingIn}>
+                <button>{loggingIn ? 'Loggin In ...' : 'Log In'}</button>
+            </div>
+        </form>
+    );
+};
 
 export default Login;
