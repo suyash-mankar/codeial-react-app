894603c273cc01cc0054aa68037664b38373dc09
suyash-mankar <suyashmankar@gmail.com>
2022-10-21 23:28:07 +0530

    setup token

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,44 +1,45 @@
-import { useContext, useState } from "react";
-import { login as userLogin } from "../api";
-import { AuthContext } from "../providers/AuthProvider";
+import { useContext, useState } from 'react';
+import { login as userLogin } from '../api';
+import { AuthContext } from '../providers/AuthProvider';
+import { setItemInLocalStorage, LOCALSTORAGE_TOKEN_KEY, removeItemFromLocalStorage } from '../utils';
 
 export const useAuth = () => {
     return useContext(AuthContext);
-}
-
+};
 
 export const useProvideAuth = () => {
-
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const login = async (email, password) => {
-
         const response = await userLogin(email, password);
 
         if (response.success) {
             setUser(response.data.user);
+            setItemInLocalStorage(
+                LOCALSTORAGE_TOKEN_KEY,
+                response.data.token ? response.data.token : null
+            );
             return {
-                success: true
-            }
+                success: true,
+            };
         } else {
             return {
                 success: false,
-                message: response.message
-            }
+                message: response.message,
+            };
         }
-
-    }
+    };
 
     const logout = () => {
         setUser(null);
-    }
+        removeItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
+    };
 
     return {
         user,
         login,
         logout,
-        loading
-    }
-
-}
+        loading,
+    };
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,33 @@
 export * from './constants';
 
+export const setItemInLocalStorage = (key, value) => {
+    if (!key || !value) {
+        console.error('cannot store in local storage');
+        return;
+    }
+    const valueToStore = typeof value !== 'string' ? JSON.stringify(value) : value;
+    localStorage.setItem(key, valueToStore);
+}
+
+
+export const getItemFromLocalStorage = (key) => {
+    if (!key) {
+        console.error('cannot get the value from local storage');
+        return;
+    }
+    localStorage.getItem(key);
+}
+
+
+export const removeItemFromLocalStorage = (key) => {
+    if (!key) {
+        console.error('cannot remove the value from local storage');
+        return;
+    }
+    localStorage.removeItem(key);
+}
+
+
 export const getFormBody = (params) => {
     let formBody = [];
 
