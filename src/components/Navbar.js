33862e2dc00545138734b235d618f4eb88dad3d2
suyash-mankar <suyashmankar@gmail.com>
2022-10-26 04:08:41 +0530

    create search UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,94 @@
 import styles from '../styles/navbar.module.css';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks';
+import { useState } from 'react';
 
 const Navbar = () => {
-    const auth = useAuth();
+  const [results, setResults] = useState([]);
+  const [searchText, setSearchText] = useState([]);
 
-    return (
-        <div className={styles.nav}>
-            <div className={styles.leftDiv}>
-                <Link to="/">
+  const auth = useAuth();
+
+  return (
+    <div className={styles.nav}>
+      <div className={styles.leftDiv}>
+        <Link to="/">
+          <img
+            alt="logo"
+            src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+          />
+        </Link>
+      </div>
+
+      <div className={styles.searchContainer}>
+        <img
+          className={styles.searchIcon}
+          src="https://cdn-icons-png.flaticon.com/512/751/751463.png"
+          alt="search"
+        />
+        <input
+          placeholder="search users"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+
+        {results.length > 0 && (
+          <div className={styles.searchResults}>
+            <ul>
+              {results.map((user) => (
+                <li
+                  className={styles.searchResultsRow}
+                  key={`user-${user._id}`}
+                >
+                  <Link to={`/users/${user._id}`}>
                     <img
-                        alt="logo"
-                        src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+                      src="https://cdn-icons-png.flaticon.com/512/2716/2716612.png"
+                      alt="user"
                     />
-                </Link>
-            </div>
+                    <span> {user.name} </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
 
-            <div className={styles.rightNav}>
-                {auth.user && (
-                    <div className={styles.user}>
-                        <Link to="/settings">
-                            <img
-                                src="https://cdn-icons-png.flaticon.com/512/924/924915.png"
-                                alt="user"
-                                className={styles.userDp}
-                            />
-                        </Link>
-                        <span> {auth.user.name} </span>
-                    </div>
-                )}
+      <div className={styles.rightNav}>
+        {auth.user && (
+          <div className={styles.user}>
+            <Link to="/settings">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/924/924915.png"
+                alt="user"
+                className={styles.userDp}
+              />
+            </Link>
+            <span> {auth.user.name} </span>
+          </div>
+        )}
 
-                <div className={styles.navLinks}>
-                    <ul>
-                        {auth.user ? (
-                            <>
-                                <li onClick={auth.logout}>
-                                    Log out
-                                </li>
-                            </>
-                        ) : (
-                            <>
-                                <li>
-                                    <Link to="/login"> Log in </Link>
-                                </li>
-                                <li>
-                                    <Link to="/register"> Register </Link>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </div>
+        <div className={styles.navLinks}>
+          <ul>
+            {auth.user ? (
+              <>
+                <li onClick={auth.logout}>Log out</li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login"> Log in </Link>
+                </li>
+                <li>
+                  <Link to="/register"> Register </Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
